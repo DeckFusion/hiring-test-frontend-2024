f23c49d09d653506ba751433f387bab23d5fe9a2
Usman-Jamil <musman.bhatti@gmail.com>
2024-07-15 01:27:18 +0500

    Restructuring the HTML files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./step1.html", "./step2.html", "./step3.html", "./step4.html"],
+  content: ["./index.html", "./src/html/*"],
   plugins: [],
 };
